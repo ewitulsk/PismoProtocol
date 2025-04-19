39d1520986f2616479712a7b06d356161815274a
fosa. <obaseki321@gmail.com>
2025-04-19 09:09:59 -0700

    mint page base

---
+++ frontend/src/app/mint/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from '@/components/mint/Header';
+
+
+const MintPage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      
+    </div>
+  );
+};
+
+export default MintPage;
