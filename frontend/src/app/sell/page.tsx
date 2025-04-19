2c8539ba32956c91a65d227c7aa70ff5e2fb299e
fosa. <obaseki321@gmail.com>
2025-04-19 09:01:32 -0700

    sell page base

---
+++ frontend/src/app/sell/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "@/components/mint/Header";
+
+
+const SellPage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <div className="p-6">
+        <h2 className="text-xl font-semibold mb-4">Your Tokens</h2>
+
+      </div>
+    </div>
+  );
+};
+
+export default SellPage;
