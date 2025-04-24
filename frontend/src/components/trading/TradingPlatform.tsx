abda080aa6b5eab33167fb9cd853aab30e6151c8
Trevor Bryant <tbryant.tbryant@gmail.com>
2025-04-23 21:46:43 -0700

    [PP-101] Trading Page Refactor
    Frontend v1.6.3
    - Adds new tabs/styling for the action tabs (positions/collateral)
    - Adds position listing underneath the chart

--- frontend/src/components/trading/TradingPlatform.tsx
+++ frontend/src/components/trading/TradingPlatform.tsx
@@ -3,25 +3,27 @@ import React from "react";
 import Layout from "../common/Layout";
 import ChartContainer from "./ChartContainer";
 import AccountHealth from "./AccountHealth";
-import CollateralPositions from "./CollateralPositions";
-import TradePanel from "./TradePanel";
+import ActionTabs from "./ActionTabs";
+import CurrentPositions from "./CurrentPositions";
 import "./trading-styles.css";
 
 const TradingPlatform: React.FC = () => {
   return (
     <Layout activePage="trading">
-      <section className="trading-layout">
-        <div className="trading-container">
-          <div className="trading-chart-wrapper">
-            <ChartContainer />
+      <div className="flex flex-col h-full overflow-y-auto">
+        <section className="trading-layout flex-grow p-4 md:p-6 lg:p-8">
+          <div className="trading-container grid grid-cols-1 lg:grid-cols-3 gap-6 mb-8">
+            <div className="trading-chart-wrapper lg:col-span-2">
+              <ChartContainer />
+            </div>
+            <aside className="trading-sidebar lg:col-span-1 flex flex-col gap-4">
+              <AccountHealth percentage={85} />
+              <ActionTabs />
+            </aside>
           </div>
-          <aside className="trading-sidebar">
-            <AccountHealth percentage={85} />
-            <CollateralPositions />
-            <TradePanel />
-          </aside>
-        </div>
-      </section>
+          <CurrentPositions />
+        </section>
+      </div>
     </Layout>
   );
 };
