1b0d22b82c8a37eddc944a373eba4eb7ada47c88
Evan Witulski <ewitulsk@calpoly.edu>
2025-07-28 15:33:38 +0300

    [PP-196] Oracle indexer expose endpoints

--- oracle_builder_indexer/src/lib.rs
+++ oracle_builder_indexer/src/lib.rs
@@ -2,6 +2,9 @@ pub mod config;
 pub mod db;
 pub mod events;
 pub mod worker;
+pub mod router;
+pub mod handlers;
 
 pub use config::Config;
-pub use worker::OracleBuilderWorker; 
+pub use worker::OracleBuilderWorker;
+pub use router::create_router; 
